b51a72205d8ea756688b7a2735e26fd547352c38
ABHIMANYU993 <abhimanyubadiger1001@gmail.com>
2024-09-21 14:18:09 +0530

    draft model training module in trained_testing_model.py

---
+++ model/training/src/models_training_scripts/trained_testing_model.py
@@ -0,0 +1,64 @@
+import torch
+from torch import nn
+from transformers import RobertaTokenizer, RobertaForSequenceClassification
+import matplotlib.pyplot as plt
+from safetensors.torch import load_file
+
+
+# Step 1: Define the CodeBERT model with Dropout
+class CodeBERTClassifier(nn.Module):
+    def __init__(self):
+        super(CodeBERTClassifier, self).__init__()
+        self.model = RobertaForSequenceClassification.from_pretrained("microsoft/codebert-base", num_labels=2)
+        self.dropout = nn.Dropout(p=0.25)  # Add a dropout layer
+
+    def forward(self, input_ids, attention_mask=None):
+        outputs = self.model(input_ids, attention_mask=attention_mask)
+        logits = self.dropout(outputs.logits)  # Apply dropout
+        return logits
+
+
+# Step 2: Load the trained model
+model = CodeBERTClassifier()
+model.load_state_dict(load_file('E:/python-projects/models_training/Novathon-JSS/model.safetensors'),strict=False)
+# model.load_state_dict(
+    # torch.load('E:/python-projects/llm/Trained_models/codebert_codeparrotjsontrained0.9886363636363636.pth'))
+# model.load_state_dict(torch.load('E:/python-projects/llm/Trained_models/codebert_primary.pth'))
+
+# model.load_state_dict(load_file('E:/python-projects/models_training/Novathon-JSS/codebert_json10-0.9587301587301588.safetensors'),strict=False)
+
+# model.load_state_dict(torch.load('E:/python-projects/llm/Trained_models/codebert_codeparrotjsontrained1.0.pth'))
+
+model.eval()  # Set the model to evaluation mode
+
+# Move model to GPU if available
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model.to(device)
+total_params = sum(p.numel() for p in model.parameters())
+print(f"Total parameters: {total_params}")
+
+# Step 3: Load the tokenizer
+tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base")
+
+
+# Step 4: Preprocess the input code samples
+def preprocess_input_code(code_samples):
+    tokenized_samples = []
+    attention_masks = []
+
+    for code_sample in code_samples:
+        tokenized_input = tokenizer(
+            code_sample,
+            padding='max_length',
+            truncation=True,
+            max_length=512,
+            return_tensors='pt'
+        )
+        tokenized_samples.append(tokenized_input['input_ids'].squeeze(0))
+        attention_masks.append(tokenized_input['attention_mask'].squeeze(0))
+
+    # Convert to PyTorch tensors
+    tokens = torch.stack(tokenized_samples)
+    masks = torch.stack(attention_masks)
+
+    return tokens, masks
