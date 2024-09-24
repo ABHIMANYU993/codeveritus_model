56f0100e5ae2776f957e5b8b1761f5889d5692fd
ABHIMANYU993 <abhimanyubadiger1001@gmail.com>
2024-09-24 16:50:08 +0530

    implement training details in main.py

--- model/training/src/models_training_scripts/main.py
+++ model/training/src/models_training_scripts/main.py
@@ -21,3 +21,26 @@ class CodeBERTClassifier(torch.nn.Module):
 def download_model_from_gdrive():
     url = 'https://drive.google.com/file/d/11_JwoS1l7Cim-NbRubhmVoE1QK3BYoy5/view?usp=drive_link'
     output = 'codebert_model.pth'
+    gdown.download(url, output, quiet=False,use_cookies=False)
+
+
+def load_model():
+    download_model_from_gdrive()
+    model = CodeBERTClassifier().to('cpu')
+    model.load_state_dict(torch.load('codebert_model.pth', map_location='cpu'))
+    model.eval()
+    tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base")
+    return model, tokenizer
+
+
+model, tokenizer = load_model()
+
+# Request model
+class CodeRequest(BaseModel):
+    userId: str
+    code_samples: list[str]
+
+# Preprocess input code
+def preprocess_input_code(code_samples):
+    inputs = tokenizer(code_samples, padding="max_length", truncation=True, max_length=512, return_tensors="pt")
+    return inputs["input_ids"], inputs["attention_mask"]
