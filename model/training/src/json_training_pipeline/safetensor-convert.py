2079c395e01b5fee3bc1a3a1ab94ca691a067e84
ABHIMANYU993 <abhimanyubadiger1001@gmail.com>
2024-10-09 14:26:11 +0530

    update safetensor-convert.py with latest corrections

--- model/training/src/json_training_pipeline/safetensor-convert.py
+++ model/training/src/json_training_pipeline/safetensor-convert.py
@@ -24,3 +24,29 @@ class CodeBERTClassifier(nn.Module):
 
 # Step 2: Load the trained model
 model = CodeBERTClassifier()
+# model.load_state_dict(torch.load('E:/python-projects/llm/Trained_models/codebert_codeparrot-v20.8955223880597015.pth'))
+# model.load_state_dict(
+# torch.load('E:/python-projects/llm/Trained_models/codebert_codeparrotjsontrained0.9886363636363636.pth'))
+model.load_state_dict(torch.load('E:/python-projects/llm/Trained_models/codebert_primary.pth'))
+
+# model.load_state_dict(
+#     load_file('E:/python-projects/models_training/Novathon-JSS/codebert_json10-0.9587301587301588.safetensors'),
+#     strict=False)
+
+# model.load_state_dict(torch.load('E:/python-projects/llm/Trained_models/codebert_codeparrotjsontrained1.0.pth'))
+state_dict = model.state_dict()
+# Define save path
+save_path = "model.safetensors"
+
+# Save in .safetensors format
+save_file(state_dict, save_path)
+model.eval()  # Set the model to evaluation mode
+
+# Move model to GPU if available
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model.to(device)
+total_params = sum(p.numel() for p in model.parameters())
+print(f"Total parameters: {total_params}")
+
+# Step 3: Load the tokenizer
+tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base")
