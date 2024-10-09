510de8bf5c8c2c1366c687a002064e9eb2ef1738
ABHIMANYU993 <abhimanyubadiger1001@gmail.com>
2024-10-09 15:21:06 +0530

    extend features in main.py

--- model/training/src/token_checking/main.py
+++ model/training/src/token_checking/main.py
@@ -30,3 +30,35 @@ public class TwoSum {
         HashMap<Integer, Integer> numToIndex = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
+            if (numToIndex.containsKey(complement)) {
+                return new int[] { numToIndex.get(complement), i };
+            }
+            numToIndex.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
+'''
+
+
+# Function to detect if the code is AI-generated or human-generated
+def detect_ai_generated_code(code_sample, human_code_sample):
+    human_perplexity = calculate_perplexity(human_code_sample)
+    ai_perplexity = calculate_perplexity(code_sample)
+
+    print(f"Human Perplexity: {human_perplexity}, AI Code Perplexity: {ai_perplexity}")
+
+    # Define a threshold for perplexity; adjust based on experimentation
+    threshold = human_perplexity * 2  # Example threshold (1.5 times the human perplexity)
+
+    if ai_perplexity >threshold:
+        return "Human-Generated Code"
+    else:
+        return "AI-Generated Code"
+
+
+# Function to read code from a text file
+def read_code_from_file(file_path):
+    with open(file_path, 'r') as file:
+        code = file.read()  # Read the entire file content
+    return code
