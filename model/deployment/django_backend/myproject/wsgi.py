7eae0441580198d99c75c6d379b8f240a4ab71cb
ABHIMANYU993 <abhimanyubadiger1001@gmail.com>
2024-11-13 17:16:58 +0530

    set up django server configuration for wsgi.py

---
+++ model/deployment/django_backend/myproject/wsgi.py
@@ -0,0 +1,26 @@
+# ==============================================================================
+# CODEVERITUS SYSTEM CORE MODULE
+# ==============================================================================
+# File: model/deployment/django_backend/myproject/wsgi.py
+# Author: ABHIMANYU993
+# Email: abhimanyubadiger1001@gmail.com
+# Project: Codeveritus - AI vs Human Code Classifier
+# Description: Modular component containing system configuration or script details.
+# ==============================================================================
+
+"""
+WSGI config for myproject project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+
+application = get_wsgi_application()
