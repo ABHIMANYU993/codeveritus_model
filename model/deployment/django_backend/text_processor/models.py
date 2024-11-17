0d897936bae7708b03bce021776ed478a89c6d9e
ABHIMANYU993 <abhimanyubadiger1001@gmail.com>
2024-11-17 19:58:36 +0530

    set up django server configuration for models.py

---
+++ model/deployment/django_backend/text_processor/models.py
@@ -0,0 +1,13 @@
+# ==============================================================================
+# CODEVERITUS SYSTEM CORE MODULE
+# ==============================================================================
+# File: model/deployment/django_backend/text_processor/models.py
+# Author: ABHIMANYU993
+# Email: abhimanyubadiger1001@gmail.com
+# Project: Codeveritus - AI vs Human Code Classifier
+# Description: Modular component containing system configuration or script details.
+# ==============================================================================
+
+from django.db import models
+
+# Create your models here.
