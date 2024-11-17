aaf3e3788228c8a2782f86c5718d7deefb52be53
ABHIMANYU993 <abhimanyubadiger1001@gmail.com>
2024-11-17 20:05:11 +0530

    set up django server configuration for serializers.py

---
+++ model/deployment/django_backend/text_processor/serializers.py
@@ -0,0 +1,14 @@
+# ==============================================================================
+# CODEVERITUS SYSTEM CORE MODULE
+# ==============================================================================
+# File: model/deployment/django_backend/text_processor/serializers.py
+# Author: ABHIMANYU993
+# Email: abhimanyubadiger1001@gmail.com
+# Project: Codeveritus - AI vs Human Code Classifier
+# Description: Modular component containing system configuration or script details.
+# ==============================================================================
+
+from rest_framework import serializers
+
+class TextInputSerializer(serializers.Serializer):
+    text = serializers.CharField()
