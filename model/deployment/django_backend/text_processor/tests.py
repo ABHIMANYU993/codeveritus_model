acab968d8fd943c082cf135fad89e04912b16b53
ABHIMANYU993 <abhimanyubadiger1001@gmail.com>
2024-11-17 20:10:06 +0530

    set up django server configuration for tests.py

---
+++ model/deployment/django_backend/text_processor/tests.py
@@ -0,0 +1,13 @@
+# ==============================================================================
+# CODEVERITUS SYSTEM CORE MODULE
+# ==============================================================================
+# File: model/deployment/django_backend/text_processor/tests.py
+# Author: ABHIMANYU993
+# Email: abhimanyubadiger1001@gmail.com
+# Project: Codeveritus - AI vs Human Code Classifier
+# Description: Modular component containing system configuration or script details.
+# ==============================================================================
+
+from django.test import TestCase
+
+# Create your tests here.
