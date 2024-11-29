12e8731bf00760ea6d2fc60ac5cace32fb11163d
ABHIMANYU993 <abhimanyubadiger1001@gmail.com>
2024-11-30 03:42:23 +0530

    extend features in app.py

--- model/deployment/streamlit_app/app.py
+++ model/deployment/streamlit_app/app.py
@@ -24,3 +24,29 @@ def download_model_from_gdrive():
     url = 'https://drive.google.com/uc?export=download&id=1FeF4C07z0kJM-9t8ra80WtU37UiiUHCe'
     output = 'codebert_model.pth'
     gdown.download(url, output, quiet=False)
+
+# Load the model and tokenizer
+@st.cache_resource  # Cache the model, so it's not reloaded on every run
+def load_model():
+    download_model_from_gdrive()
+    model = CodeBERTClassifier().to(device)
+    model.load_state_dict(torch.load('codebert_model.pth', map_location=device))
+    model.eval()
+    tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base")
+    return model, tokenizer
+
+# Preprocess the code samples
+def preprocess_input_code(code_samples, tokenizer):
+    tokenized_samples = []
+    attention_masks = []
+
+    for code_sample in code_samples:
+        tokenized_input = tokenizer(
+            code_sample,
+            padding='max_length',
+            truncation=True,
+            max_length=512,
+            return_tensors='pt'
+        )
+        tokenized_samples.append(tokenized_input['input_ids'].squeeze(0))
+        attention_masks.append(tokenized_input['attention_mask'].squeeze(0))
