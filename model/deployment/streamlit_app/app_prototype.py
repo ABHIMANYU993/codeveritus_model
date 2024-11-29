bd7d8805df5bf2803010d178a3e0671123f4f2f6
ABHIMANYU993 <abhimanyubadiger1001@gmail.com>
2024-11-30 05:07:48 +0530

    extend features in app_prototype.py

--- model/deployment/streamlit_app/app_prototype.py
+++ model/deployment/streamlit_app/app_prototype.py
@@ -29,3 +29,34 @@ class CodeBERTClassifier(nn.Module):
         self.dropout = nn.Dropout(p=0.3)
 
     def forward(self, input_ids, attention_mask=None):
+        outputs = self.model(input_ids, attention_mask=attention_mask)
+        logits = self.dropout(outputs.logits)
+        return logits
+
+
+def download_model_from_gdrive():
+    url = 'https://drive.google.com/uc?export=download&id=1FeF4C07z0kJM-9t8ra80WtU37UiiUHCe'
+    output = 'codebert_model.pth'
+    gdown.download(url, output, quiet=False)
+
+
+# Preprocess the code sample
+def preprocess_input_code(code_samples, tokenizer):
+    tokenized_samples = []
+    attention_masks = []
+
+    for code_sample in code_samples:
+        tokenized_input = tokenizer(
+            code_sample,
+            padding='max_length',
+            truncation=True,
+            max_length=512,
+            return_tensors='pt'
+        )
+        tokenized_samples.append(tokenized_input['input_ids'].squeeze(0))
+        attention_masks.append(tokenized_input['attention_mask'].squeeze(0))
+
+    # Convert to PyTorch tensors
+    tokens = torch.stack(tokenized_samples)
+    masks = torch.stack(attention_masks)
+    return tokens, masks
