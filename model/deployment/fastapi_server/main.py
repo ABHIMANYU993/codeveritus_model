c61d1afb246cf84384ae1ddcfc96e65e00df59c3
ABHIMANYU993 <abhimanyubadiger1001@gmail.com>
2024-11-30 01:56:49 +0530

    extend features in main.py

--- model/deployment/fastapi_server/main.py
+++ model/deployment/fastapi_server/main.py
@@ -19,3 +19,26 @@ class CodeBERTClassifier(nn.Module):
         logits = self.dropout(outputs.logits)
         return logits
 
+
+model = CodeBERTClassifier()
+tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base")
+model.load_state_dict(torch.load('codebert_model.pth',map_location=torch.device('cpu')))
+model.eval()
+# Move model to GPU if available
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cpu')
+model.to(device)
+
+
+# Define input data model
+class CodeInput(BaseModel):
+    code_samples: list
+
+
+@app.post("/predict/")
+def predict_code(code_input: CodeInput):
+    code_samples = code_input.code_samples
+    # Tokenization and prediction logic here
+    tokenized_samples = []
+    attention_masks = []
+    for code_sample in code_samples:
