e13bb6238d7f1c2dffb9b6082ee3d3c240cdb2a9
ABHIMANYU993 <abhimanyubadiger1001@gmail.com>
2024-12-11 08:26:10 +0530

    finalize huggingface web application code

--- model/deployment/hf_space/app.py
+++ model/deployment/hf_space/app.py
@@ -27,3 +27,33 @@ class CodeBERTClassifier(torch.nn.Module):
     def forward(self, input_ids, attention_mask=None):
         outputs = self.model(input_ids, attention_mask=attention_mask)
         return outputs.logits
+
+
+def load_model():
+    model = CodeBERTClassifier()
+    model.load_state_dict(load_file('model.safetensors'), strict=False)
+    model.eval()
+    tokenizer = RobertaTokenizer.from_pretrained(
+        "microsoft/codebert-base",
+        cache_dir=os.environ["HF_HOME"]
+    )
+    return model, tokenizer
+
+model, tokenizer = load_model()
+
+
+class CodeRequest(BaseModel):
+    code_samples: list[str]
+
+
+def preprocess_input_code(code_samples):
+    inputs = tokenizer(code_samples, padding="max_length", truncation=True, max_length=512, return_tensors="pt")
+    return inputs["input_ids"].to(device), inputs["attention_mask"].to(device)  # Move tensors to CPU
+
+
+def predict(code_samples):
+    tokens, masks = preprocess_input_code(code_samples)
+    with torch.no_grad():
+        logits = model(tokens, attention_mask=masks)
+        probabilities = torch.nn.functional.softmax(logits, dim=1).numpy()  # Keep on CPU for processing
+    return probabilities
