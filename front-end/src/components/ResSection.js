5f926d84b5f7c7ae42fadddf95fd5479e8ac2015
ABHIMANYU993 <abhimanyubadiger1001@gmail.com>
2025-01-04 20:13:01 +0530

    create react component template for ResSection

---
+++ front-end/src/components/ResSection.js
@@ -0,0 +1,28 @@
+/*
+ * ==============================================================================
+ * CODEVERITUS SYSTEM CORE MODULE
+ * ==============================================================================
+ * File: front-end/src/components/ResSection.js
+ * Author: ABHIMANYU993
+ * Email: abhimanyubadiger1001@gmail.com
+ * Project: Codeveritus - AI vs Human Code Classifier
+ * Description: Modular component containing system configuration or script details.
+ * ==============================================================================
+ */
+
+import React from 'react'
+
+const ResSection = () => {
+  return (
+    <div className="container my-5" >
+    <h2 className="text-center fw-bold mb-4" style={{color: 'rgb(220, 81, 81)' }}>
+      Proven Results You Can Count On
+    </h2>
+    <p className="lead text-center" style={{ color: 'rgb(239, 227, 227)' }}>
+    Save 90% of the time spent manually reviewing code. Our AI-driven platform detect even the most advanced code imitation. Foster a culture of originality and innovation effortlessly.
+    </p>
+  </div>
+  )
+}
+
+export default ResSection
