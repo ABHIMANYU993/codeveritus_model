1e7c0356c86a61905a8c014939268631d88d1139
ABHIMANYU993 <abhimanyubadiger1001@gmail.com>
2025-01-24 10:19:27 +0530

    add styling and logic to Navbar component

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -19,3 +19,25 @@ const Navbar = () => {
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
         >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item mx-4">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">
+                Login
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
