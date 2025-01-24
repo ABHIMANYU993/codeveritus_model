607901a6c8a1eb365c9c398678b7b1d039b9d515
ABHIMANYU993 <abhimanyubadiger1001@gmail.com>
2025-01-24 06:29:20 +0530

    add styling and logic to Footer component

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -14,3 +14,19 @@ const Footer = () => {
         </a>
         <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="mx-2">
           <FontAwesomeIcon icon={faTwitter} size="2x" style={{ color: '#1da1f2' }} />
+        </a>
+        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="mx-2">
+          <FontAwesomeIcon icon={faLinkedin} size="2x" style={{ color: '#0077b5' }} />
+        </a>
+        <a href="https://github.com" target="_blank" rel="noopener noreferrer" className="mx-2">
+          <FontAwesomeIcon icon={faGithub} size="2x" style={{ color: 'white' }} />
+        </a>
+      </div>
+      <p className="mb-0">
+        © {new Date().getFullYear()} CodeVeritus. All Rights Reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
